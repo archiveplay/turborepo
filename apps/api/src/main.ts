7752603cbb2cd89bd947503ecd6ae3fbddeebdaa
Vladislav Nefedov <clay.zenx@gmail.com>
2026-06-05 17:22:02 +0300

    feat(apps/api): swagger api generator

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from '@tooling/env/server';
+import { makeSwagger } from './common/utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  makeSwagger(app);
   await app.listen(env.PORT);
 }
 bootstrap().catch(console.error);
