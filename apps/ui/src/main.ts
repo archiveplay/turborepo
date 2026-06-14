2b64676e5e7b5e493d39c3f0de5ee456eb371f5f
Vladislav Nefedov <clay.zenx@gmail.com>
2026-06-14 19:25:04 +0300

    feat(apps/ui): move fsd like files

--- apps/ui/src/main.ts
+++ apps/ui/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import router from './app/router'
+import App from './app/App.vue'
 
 const app = createApp(App)
 
