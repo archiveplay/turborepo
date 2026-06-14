bec72c83f66b3553e654d3ee0231948f298f85d9
Vladislav Nefedov <clay.zenx@gmail.com>
2026-06-14 19:43:17 +0300

    refactor(app/api): rename app

---
+++ apps/web/src/app/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { userControllerFindUnique } from '@pkg/api/client/vue'
+
+const { data } = await userControllerFindUnique({ where: { id: 1 } })
+console.log(data)
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <pre>
+    {{ data }}
+  </pre>
+</template>
+
+<style scoped></style>
