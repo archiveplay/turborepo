1af0aab0686bc81285b6a6738f778cee79b70fad
Vladislav Nefedov <clay.zenx@gmail.com>
2026-06-26 15:36:29 +0300

    feat(apps/nuxt): use-user api entity

---
+++ apps/nuxt/app/pages/user/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useUser } from "~/entities/user/api/use-user";
+
+const route = useRoute();
+
+const id = computed(() => Number(route.params.id));
+
+const { data, isPending, error } = useUser(id.value);
+</script>
+
+<template>
+  <h1>User</h1>
+
+  <div v-if="isPending">Loading...</div>
+
+  <div v-else-if="error">Error: {{ error }}</div>
+
+  <pre v-else>
+    {{ data }}
+  </pre>
+</template>
